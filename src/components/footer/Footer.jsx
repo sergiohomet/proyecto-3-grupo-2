40eac4d595cd6e49d669b0e39f031c061601c7ef
ulises-burgos <ulisesgaburgos@gmail.com>
2023-09-30 17:40:38 -0300

    estructura del footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,1 +1,41 @@
 import React from 'react'
+
+
+
+const Footer = () => {
+    return (
+      <div className='main-footer'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-3 col-sm-6'>
+              <h4>Lorem, ipsum.</h4>
+              <ul className='list-unstyled'>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            <div className='col-md-3 col-sm-6'>
+              <h4>Lorem, ipsum.</h4>
+              <ul className='list-unstyled'>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            
+  
+          </div>
+          {/*boton del footer*/}
+          <div className='footer-bottom'>
+            <p className='text-xs-enter'>
+            &copy;{new Date().getFullYear()}  Esta pagina es del equipo 2
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    )
+  }
+  
+  export default Footer
