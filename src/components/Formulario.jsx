36ed9162255a305643b0663cfb1e6f0d5557926c
ulises-burgos <ulisesgaburgos@gmail.com>
2023-10-04 20:13:03 -0300

    componente formulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './Formulario.css'
+
+export function Formulario ({setUser}) {
+    const [Nombre, setNombre] = useState ("")
+    const [Contraseña, setContraseña] = useState ("")
+    const [Error, setError] = useState (false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+if (Nombre ==="" || Contraseña ===""){
+    setError(true)
+    return
+}
+
+setError(false)
+
+setUser([Nombre])
+
+    } 
+
+    return (
+        <section>
+            <h1>Login</h1>
+
+            <form className='Formulario'
+            onSubmit={handleSubmit}>
+                
+                <input type="text"
+                value={Nombre}
+                onChange={e =>setNombre(e.target.value)}
+                /><a>Ingrese nombre de usuario</a>
+                
+                <input type="Password"
+                value={Contraseña}
+                onChange={e =>setContraseña(e.target.value)}
+                /><a>Ingrese contraseña</a>
+
+                <button>Iniciar Sesión</button>
+                
+                
+            </form> 
+            {Error &&<p>Todos los campos son obligatorios</p>}
+
+
+
+        </section>
+    )
+
+
+
+
+}
