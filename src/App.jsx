181717a77acf2c4e496652aff8b8aa0184adad6d
ulises-burgos <ulisesgaburgos@gmail.com>
2023-10-04 20:18:47 -0300

    app login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useDeferredValue, useState } from 'react'
+import { Formulario } from './components/formulario'
+import { Home } from './components/Home'
 
+import './App.css'
 
 function App() {
+
+   const [user, setUser] = useState ([])
   
 
   return (
-    <>
+    
+      <div className="App">
+        {
+          !user.length > 0
+          ?<Formulario setUser={setUser} />
+          : <Home user={user} setUser={setUser} />
+
+
+        }
+        
+        
+      </div>
       
-    </>
+      
+      
+
   )
 }
 
