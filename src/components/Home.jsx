f33369dd251c6502d3889d56ecab20053b5938db
ulises-burgos <ulisesgaburgos@gmail.com>
2023-10-04 20:15:15 -0300

    exportando y home

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+export function Home({user, setUser}) {
+
+    const handleLogout = () => {
+
+        setUser([])
+    }
+
+
+    return (
+        <div>
+           <h1> Bienvenido</h1>
+           <h2>{user}</h2>
+           <button onClick={handleLogout}>Cerrar Sesion</button>
+
+        </div>
+    )
+}
