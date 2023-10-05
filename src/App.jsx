af4d54338286132e983734a0c2caf3064508e224
ulises-burgos <ulisesgaburgos@gmail.com>
2023-10-05 17:49:07 -0300

    estructura formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import Registro from "./components/Registro"
+import React from "react"
+import { Route, Routes } from "react-router-dom";
 
 
 function App() {
-  
+ 
 
   return (
-    <>
+    
+      <div className='App'>
+        <NavBarpagina />
+        
+        
+        {/* <Router> */}
+          
+        <Routes>
+           
+            <Route path='/Registro' element={<Registro/>}/>
+            </Routes>
+        {/* </Router> */}
+       
+        <Footer/>
+      </div>
       
-    </>
-  )
+    
+  );
 }
 
 export default App
---
+++ src/components/Registro.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Registro = () => {
+  return (  
+
+
+
+    <div>
+        <form>
+        
+             <label className='form-label'>Nombre</label>
+             <input type='text' className='form-control'/>
+             <label className='form-label'>Apellido</label>
+             <input type='text' className='form-control'/>
+             <label className='form-label'>Telefono</label>
+             <input type='text' className='form-control'/>
+             <label className='form-label'>Correo Electronico</label>
+             <input type='text' className='form-control'/>
+             <label className='form-label'>Contraseña</label>
+             <input type='text' className='form-control'/>
+             <label className='form-label'>Confirmar Contraseña</label>
+             <input type='text' className='form-control'/>
+             <textarea className='form-control'/>
+
+
+
+        </form> 
+        
+        
+        </div>
+
+
+
+  )
+}
+
+export default Registro
