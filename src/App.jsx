5617ded3c3eeb2ca6788887708d71fc644856a5c
Cynthia Pistan <cynthiapistan@gmail.com>
2023-10-06 18:29:40 -0300

    pagina de error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
+import React from 'react'
+import Error404 from "./Components/Error404.jsx"
 
 function App() {
-  
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Error404 />
+    </div>
   )
 }
 
 export default App
+ 
---
+++ src/Components/Error404.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import "./Error404.css"
+
+const Error404 = () => {
+  return ( 
+    <div className="main-container">
+      <div className="container-img">
+        <Image className="img mx-auto" src="./src/assets/dog.jpg" fluid ></Image>
+          <p className="number mx-auto ">404
+          </p>
+      </div>
+      <div className="container-phrase">
+        <p className="container-error py-4">
+          Upsss!!! Nada puede malir sal. 
+          Página No Encontrada.
+        </p>
+        <Button className='button-error404'><b>PAGINA DE INICIO</b></Button>
+        <div className="burbujas">
+          <div className="burbuja"></div>
+          <div className="burbuja"></div>
+          <div className="burbuja"></div>
+          <div className="burbuja"></div>
+          <div className="burbuja"></div>
+          <div className="burbuja"></div>
+          <div className="burbuja"></div>
+          <div className="burbuja"></div>
+        </div>
+      </div>
+    </div>
+   
+  )
+}
+
+export default Error404;
