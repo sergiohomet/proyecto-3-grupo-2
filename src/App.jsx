90c42fcbde4861922829dd820d56111be8cc4303
Cynthia Pistan <cynthiapistan@gmail.com>
2023-10-06 18:43:54 -0300

    creada pagina de nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
+import React from 'react'
+import AcercaDeNosotros from "./Components/AcercaDeNosotros.jsx"
+import "./Components/AcercaDeNosotros.css"
 
 function App() {
-  
 
   return (
-    <>
-      
-    </>
+    
+    <div className='App'>
+      <AcercaDeNosotros />
+    </div>
   )
 }
 
