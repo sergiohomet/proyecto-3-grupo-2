db603ae1349b4d900803845a22cdd42ef09c665d
goncalhurtado <gonzalomilano@gmail.com>
2023-10-06 20:23:07 -0300

    agregue informacion de planes para el envio de mail

--- src/data/planesBack.js
+++ src/data/planesBack.js
@@ -1,14 +1,8 @@
-export const planes = [
+export const planesBack = [
     //
     {
         id: 1,
         titulo: "Basico",
-        caracteristicas: [
-            "100%off en consultas y clinica general horario diurno",
-            "10%off en alimentos y accesorios",
-            "20%off farmacia",
-            "20oof en cardiologia"
-        ],
         infoPlan: {
             "consulta y controles clinica general (horario diurno)": "100%",
             "colocacion de microchip": "25%",
@@ -35,13 +29,6 @@ export const planes = [
     {
         id: 2,
         titulo: "Estandar (Recomendado)",
-        caracteristicas: [
-            "100%off en consultas y clinica general horario diurno",
-            "15%off en alimentos y accesorios",
-            "30%off farmacia",
-            "50%off baño y corte",
-            "40%off en cardiologia, dermatologia oftalmologia y mas"
-        ],
         infoPlan: {
             "consulta y controles clinica general (horario diurno)": "100%",
             "colocacion de microchip": "50%",
@@ -68,13 +55,6 @@ export const planes = [
     {
         id: 3,
         titulo: "Premium",
-        caracteristicas: [
-            "100%off en consultas y clinica general horario diurno",
-            "20%off en alimentos y accesorios",
-            "40%off farmacia",
-            "50%off baño y corte",
-            "100%off en cardiologia, dermatologia oftalmologia y mas"
-        ],
         infoPlan: {
             "consulta y controles clinica general (horario diurno)": "100%",
             "colocacion de microchip": "100%",
@@ -96,7 +76,6 @@ export const planes = [
             "alimentos y accesorios": "20%",
             "farmacia": "40%",
             "baño y corte": "50%"
-
         }
     }
 ];
