2c0c3dac8a9ec1c4bb2425e60f2175165a60acc0
Sergio Homet <103963551+sergiohomet@users.noreply.github.com>
2023-10-07 17:37:47 -0300

    Registro (#6)
    * creando formulario
    
    * formulario registro
    
    * estructura formulario
    
    * formulario-hooks
    
    * seteado
    
    * correciones registro
    
    * registro sin validar
    
    * iniciando validaciones
    
    * error de alertas
    
    * Correccion de sintaxis
    
    * Pagina añadida a rutas
    
    ---------
    
    Co-authored-by: ulises-burgos <ulisesgaburgos@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Footer from "./components/footer/Footer";
 import AdminPacientes from "./pages/AdminPacientes";
 import AdminTurnos from "./pages/AdminTurnos";
 import AdminIndex from "./pages/AdminIndex";
-import "./App.css";
+import "./app.css";
+import Registro from "./pages/Registro";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/registro" element={<Registro />} />
           <Route path="/pacientes" element={<AdminPacientes />} />
           <Route path="/turnos" element={<AdminTurnos />} />
           <Route path="/admin" element={<AdminIndex />} />
