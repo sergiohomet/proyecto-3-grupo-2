1b125ef6dfbb9d4e2d1b0d24d49ea63638175e11
ulises-burgos <ulisesgaburgos@gmail.com>
2023-10-10 00:47:21 -0300

    colores al footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,21 +3,21 @@ import styled from "styled-components";
 
 const Footer = () => {
     return (
-      <FooterContainer className="main-footer">
+      <FooterContainer className="footer-container">
         <div className="footer-middle">
           <div className="container">
             <div className="row">
 
             <div className="col-md-3 col-sm-6 mx-auto">
             <ul className="list-unstyled">
-                <li><img src='./public/patitas_logo.png'width={'120'} alt="" ></img></li>
+                <li><img src='./public/Logo de Patitas Vet.png'width={'250'} alt="" ></img></li>
                 </ul>
             </div>
 
               <div className="col-md-3 col-sm-6 mx-auto">
-                <h4>Veterinaria Patitas</h4>
+                <h4 className="h4-footer">Veterinaria Patitas</h4>
                 <ul className="list-unstyled">
-                  <li><a href="/"> Acerca de Nosotros</a></li>
+                  <li><a href= "/"> Acerca de Nosotros</a></li>
                   <li>telefono de contacto - 0800-Martin-Conty-555</li>
                   <li><a href="/">Ubicacion</a></li>
                   <li>correo-electronico@patitas.com</li>
@@ -25,12 +25,12 @@ const Footer = () => {
                 </ul>
               </div>
               <div className="col-md-3 col-sm-6 mx-auto">
-                <h4>redes sociales</h4>
+                <h4 className="h4-footer">Redes sociales</h4>
                 <ul className="list-unstyled">
-                <li><a href="/" ><i className="bi bi-facebook"></i></a>Facebook</li>
-                <li><a href="/" ><i className="bi bi-instagram"></i></a>Instagram</li>
-                <li><a href="/" ><i className="bi bi-twitter"></i></a>Twitter</li>
-                <li><a href="/" ><i className="bi bi-tiktok"></i></a>Tik-Tok</li>
+                <li><a href="/"><i className="bi bi-facebook"></i></a>Facebook</li>
+                <li><a href="/"><i className="bi bi-instagram"></i></a>Instagram</li>
+                <li><a href="/"><i className="bi bi-twitter"></i></a>Twitter</li>
+                <li><a href="/"><i className="bi bi-tiktok"></i></a>Tik-Tok</li>
                 </ul>
               </div>
             </div>
