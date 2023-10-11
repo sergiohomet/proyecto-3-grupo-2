a69d3bb56ebbd6476d3363d00f475bfedc8f1173
goncalhurtado <gonzalomilano@gmail.com>
2023-10-11 01:05:27 -0300

    agregue loader y mostrarle error al usuario en form planes

--- src/components/Servicios/ServiciosGrid.jsx
+++ src/components/Servicios/ServiciosGrid.jsx
@@ -6,7 +6,10 @@ const ServiciosGrid = () => {
   return (
     <>
       {servicios.map((data) => (
-        <div key={data.id} className="serviciosCont col-6 col-sm-6 col-lg-3">
+        <div
+          key={data.id}
+          className="serviciosCont col-6 col-sm-6 col-lg-3 mt-2 mt-lg-0"
+        >
           <ServiciosCards data={data} key={data.id} />
         </div>
       ))}
