f1af3f8bc46fa6f4d344823229d63b3ea77a94c7
goncalhurtado <gonzalomilano@gmail.com>
2023-10-10 21:23:27 -0300

    probando axios envio mail

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+    baseURL: "https://backend-veterinaria-patitas.onrender.com/api/veterinaria",
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer: ${token}`;
-  }
-  return config;
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer: ${token}`;
+    }
+    return config;
 });
