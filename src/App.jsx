1ad1b0a19c5ea7e8dd11a69f9bc852e6c46b9c2d
Sergio Homet <103963551+sergiohomet@users.noreply.github.com>
2023-10-12 01:32:19 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Index from "./pages";
 import { Route, Routes } from "react-router-dom";
-import Footer from "./Components/footer/Footer";
 import AdminPacientes from "./pages/AdminPacientes";
 import AdminTurnos from "./pages/AdminTurnos";
 import AdminIndex from "./pages/AdminIndex";
-import Nosotros from "./pages/Nosotros";
 import Login from "./pages/Login";
 import PrivateRoutes from "./Routes/PrivateRoutes";
 import { useEffect, useState } from "react";
 import Contacto from "./pages/Contacto";
 import NavBarpagina from "./components/navegacion/NavBarpagina";
 import ErrorPage from "./pages/ErrorPage";
+import Footer from "./Components/footer/Footer";
+import AcercaDeNosotros from "./Components/AcercaDeNosotros";
 
 function App() {
   const [isLogged, setIsLogged] = useState(
@@ -27,18 +27,21 @@ function App() {
       <div className="app-container">
         <NavBarpagina setIsLogged={setIsLogged} isLogged={isLogged} />
         <div className="content">
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<ErrorPage />} />
-          <Route path="/nosotros" element={<Nosotros />} />
-          <Route path="/contacto" element={<Contacto />} />
-          <Route path="/login" element={<Login setIsLogged={setIsLogged} />} />
-          <Route element={<PrivateRoutes />}>
-            <Route path="/pacientes" element={<AdminPacientes />} />
-            <Route path="/turnos" element={<AdminTurnos />} />
-            <Route path="/admin" element={<AdminIndex />} />
-          </Route>
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<ErrorPage />} />
+            <Route path="/nosotros" element={<AcercaDeNosotros />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route
+              path="/login"
+              element={<Login setIsLogged={setIsLogged} />}
+            />
+            <Route element={<PrivateRoutes />}>
+              <Route path="/pacientes" element={<AdminPacientes />} />
+              <Route path="/turnos" element={<AdminTurnos />} />
+              <Route path="/admin" element={<AdminIndex />} />
+            </Route>
+          </Routes>
         </div>
         <Footer />
       </div>
